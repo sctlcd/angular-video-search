688098505cc2f6186c76cfcce531926a2d72b3de
8 <lucinda.souchet@outlook.com>
2021-07-31 17:04:17 +0200

    feat: update search input component

--- src/app/search-bar/search-input/search-input.component.ts
+++ src/app/search-bar/search-input/search-input.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, pluck, distinctUntilChanged, filter, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'searchInput',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.css']
 })
-export class SearchInputComponent implements OnInit {
+export class SearchInputComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild('input') inputElement: ElementRef;
+  @Output() search: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    fromEvent(this.inputElement.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(500),
+        pluck('target', 'value'),
+        distinctUntilChanged(),
+        filter((value: string) => value.length > 3),
+        map((value) => value)
+      )
+      .subscribe(value => {
+        this.search.emit(value);
+      });
   }
 
 }
